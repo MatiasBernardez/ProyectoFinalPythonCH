4c82b955b0304a6026714ba24ec453d593bf89d4
Matias Bernardez <matiasbernardez10@gmail.com>
2023-03-06 18:13:30 -0300

    BarrasNav

--- AppBlog/urls.py
+++ AppBlog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from AppBlog import views
 
 urlpatterns = [
-    path('', views.inicio),
+    path('', views.inicio, name="inicio"),
+    path('especialidad', views.especialidad, name="especialidad"),
+    path('modalidadatencion', views.modalidadatencion, name="modalidadatencion"),
+    path('orientacion', views.orientacion, name="orientacion"),
+    path('tipotratamiento', views.tipotratamiento, name="tipotratamiento"),
+    path('zonaatencion', views.zonaatencion, name="zonaatencion"),
 ]
--- AppBlog/views.py
+++ AppBlog/views.py
@@ -6,3 +6,23 @@ from django.http import HttpResponse
 def inicio(request):
 
     return render (request, "AppBlog/inicio.html")
+
+def especialidad(request):
+
+    return render (request, "AppBlog/especialidad.html")
+
+def modalidadatencion(request):
+
+    return render (request, "AppBlog/modalidadatencion.html")
+
+def orientacion(request):
+
+    return render (request, "AppBlog/orientacion.html")
+
+def tipotratamiento(request):
+
+    return render (request, "AppBlog/tipotratamiento.html")
+
+def zonaatencion(request):
+
+    return render (request, "AppBlog/zonaatencion.html")
